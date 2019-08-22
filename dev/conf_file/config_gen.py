0b8537c8a58c0bacb4a9760f5fe846425cc6d5c9
Daniel <danielmacuare.uk@gmail.com>
2019-08-22 08:04:46 +0100

    Ignoring pycache and virtualenv

---
+++ dev/conf_file/config_gen.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/python3
+from configparser import ConfigParser, ExtendedInterpolation
+from pathlib import Path
+
+working_dir = Path.cwd()
+
+def read_config_file():
+    parser = ConfigParser(interpolation=ExtendedInterpolation())
+    parser.read()
+    print(parser.sections())
+    return None
+
+def create_default_conf():
+    conf_file_path = f'{working_dir}/wedev.ini'
+    log_file_path = f'{working_dir}/webdev.log'
+    
+    config = ConfigParser()
+    print(f'Current working dir{Path.cwd()}')
+    config['settings'] = {
+        'packages_file': 'packages.txt', 
+        'log_path': str(log_file_path)
+    }
+
+    with open (conf_file_path, 'w') as f:
+        config.write(f)
+    print(f'A config file has been created at --> "{conf_file_path}"')
+    return None
---
+++ dev/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env/python3
+from conf_file import config_gen
+
+
+if __name__ == '__main__':
+    config_gen.create_default_conf()
